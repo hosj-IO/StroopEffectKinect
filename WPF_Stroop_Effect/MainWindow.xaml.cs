d199bae79d9a2bf4017a3ea133f390ae4e1fffa5
Sjoerd Houben <hosjEU@users.noreply.github.com>
2013-04-08 15:20:49 +0200

    Stroop effect Same color & text is NOT prevented

--- WPF_Stroop_Effect/MainWindow.xaml.cs
+++ WPF_Stroop_Effect/MainWindow.xaml.cs
@@ -19,9 +19,64 @@ namespace WPF_Stroop_Effect
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<string> colorNames;
+        private List<Color> colors;
+        private int index = 0, indexC = 0;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitColors();
+            InitNames();
+            NextColor();
+        }
+
+
+        #region Methods
+        private void InitColors()
+        {
+            colors = new List<Color>();
+            colors.Add(Colors.Red);
+            colors.Add(Colors.Blue);
+            colors.Add(Colors.Green);
+        }
+
+        private void InitNames()
+        {
+            colorNames = new List<string>();
+            colorNames.Add("Red");
+            colorNames.Add("Blue");
+            colorNames.Add("Green");
+        }
+        #endregion
+
+        private void textBlock1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            NextColor();
+        }
+
+        private void NextColor()
+        {
+            Random rdm = new Random();
+            int i;
+            do
+            {
+                i = rdm.Next(0, colorNames.Count);
+            } while (i == index);
+
+            index = i;
+
+            textBlock1.Text = colorNames[index];
+
+            int j;
+            do
+            {
+                j = rdm.Next(0, colors.Count);
+            } while (j == indexC);
+            indexC = j;
+
+            textBlock1.Foreground = new SolidColorBrush(colors[indexC]);
+
         }
     }
 }

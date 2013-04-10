32588b055daf2043f51bb9d363974a95eb8c12fd
hosj <hosj@localhost>
2013-04-10 16:00:07 +0200

    Alpha
    Voice commands intergrated

--- WPF_Stroop_Effect/MainWindow.xaml.cs
+++ WPF_Stroop_Effect/MainWindow.xaml.cs
@@ -12,6 +12,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Kinect;
+using System.Threading;
+using Microsoft.Speech.Recognition;
+using System.IO;
+using Microsoft.Speech.AudioFormat;
+using System.Diagnostics;
+using System.ComponentModel;
+
 namespace WPF_Stroop_Effect
 {
     /// <summary>
@@ -19,19 +27,57 @@ namespace WPF_Stroop_Effect
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        #region Variables
         private List<string> colorNames;
         private List<Color> colors;
-        private int index = 0, indexC = 0;
+        private int index = 0, indexC = 0, score = 0;
 
+        KinectSensor _kinectSensor;
+        SpeechRecognitionEngine _sre;
+        KinectAudioSource _source;
+
+        #endregion
+
+        #region Constructor
         public MainWindow()
         {
             InitializeComponent();
+            textBlockScore.Text = score.ToString();
+
             InitColors();
             InitNames();
             NextColor();
             CreateShade();
+            this.DataContext = this;
+            this.Unloaded += delegate
+            {
+                _kinectSensor.SkeletonStream.Disable();
+                _sre.RecognizeAsyncCancel();
+                _sre.RecognizeAsyncStop();
+                //_source.Dispose();
+                _sre.Dispose();
+            };
+            this.Loaded += delegate
+            {
+                _kinectSensor = KinectSensor.KinectSensors[0];
+                _kinectSensor.SkeletonStream.Enable(new TransformSmoothParameters()
+                {
+                    Correction = 0.5f
+                    ,
+                    JitterRadius = 0.05f
+                    ,
+                    MaxDeviationRadius = 0.04f
+                    ,
+                    Smoothing = 0.5f
+                });
+                _kinectSensor.Start();
+                StartSpeechRecognition();
+            };
         }
+        #endregion
 
+        #region Methods
         private void CreateShade()
         {
             //TextBlock textBlockShade = new TextBlock();
@@ -43,9 +89,6 @@ namespace WPF_Stroop_Effect
             //textBlockShade.Foreground = Brushes.Black;
             //Canvas.SetZIndex(textBlockShade, (int)1);
         }
-
-
-        #region Methods
         private void InitColors()
         {
             colors = new List<Color>();
@@ -71,7 +114,7 @@ namespace WPF_Stroop_Effect
             colorNames.Add("Black");
             colorNames.Add("Green");
         }
-        #endregion
+
 
         private void textBlock1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
@@ -100,7 +143,146 @@ namespace WPF_Stroop_Effect
             indexC = j;
 
             textBlock1.Foreground = new SolidColorBrush(colors[indexC]);
+            textBlockScore.Text = score.ToString();
 
         }
+
+
+        private void WordGuessCorrect()
+        {
+            score++;
+            NextColor();
+            CreateShade();
+        }
+
+
+        #endregion
+
+        #region KinectMethods
+        private KinectAudioSource CreateAudioSource()
+        {
+            var source = KinectSensor.KinectSensors[0].AudioSource;
+            source.AutomaticGainControlEnabled = false;
+            source.EchoCancellationMode = EchoCancellationMode.None;
+            return source;
+        }
+
+        private void StartSpeechRecognition()
+        {
+            _source = CreateAudioSource();
+
+            Func<RecognizerInfo, bool> matchingFunc = r =>
+            {
+                string value;
+                r.AdditionalInfo.TryGetValue("Kinect", out value);
+                return "True".Equals(value, StringComparison.InvariantCultureIgnoreCase)
+                    && "en-US".Equals(r.Culture.Name, StringComparison.InvariantCultureIgnoreCase);
+            };
+            RecognizerInfo ri = SpeechRecognitionEngine.InstalledRecognizers().Where(matchingFunc).FirstOrDefault();
+
+            _sre = new SpeechRecognitionEngine(ri.Id);
+            CreateGrammars(ri);
+            _sre.SpeechRecognized += sre_SpeechRecognized;
+            _sre.SpeechHypothesized += sre_SpeechHypothesized;
+            _sre.SpeechRecognitionRejected += sre_SpeechRecognitionRejected;
+
+            Stream s = _source.Start();
+            _sre.SetInputToAudioStream(s,
+                                        new SpeechAudioFormatInfo(
+                                            EncodingFormat.Pcm, 16000, 16, 1,
+                                            32000, 2, null));
+            _sre.RecognizeAsync(RecognizeMode.Multiple);
+        }
+
+        private void CreateGrammars(RecognizerInfo ri)
+        {
+            var commands = new Choices();
+            foreach (String colorName in colorNames)
+            {
+                commands.Add(colorName);
+            }            
+
+            var gb = new GrammarBuilder();
+            gb.Culture = ri.Culture;
+            gb.Append(commands);
+
+            var g = new Grammar(gb);
+            _sre.LoadGrammar(g);
+
+            var q = new GrammarBuilder();
+            q.Append("quit");
+            var quit = new Grammar(q);
+
+            _sre.LoadGrammar(quit);
+        }
+
+        void sre_SpeechRecognitionRejected(object sender, SpeechRecognitionRejectedEventArgs e)
+        {
+            HypothesizedText += " Rejected";
+            Confidence = Math.Round(e.Result.Confidence, 2).ToString();
+        }
+
+        void sre_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            HypothesizedText = e.Result.Text;
+        }
+
+        void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action<SpeechRecognizedEventArgs>(InterpretCommand), e);
+        }
+
+        private void InterpretCommand(SpeechRecognizedEventArgs e)
+        {
+            var result = e.Result;
+            Confidence = Math.Round(result.Confidence, 2).ToString();
+            //if (result.Confidence < 95 && result.Words[0].Text == "quit")
+            //{
+            //    this.Close();
+            //}
+
+
+            if (result.Words[0].Text == colorNames[indexC])
+            {
+                WordGuessCorrect();   
+            }
+
+
+        }
+
+
+        private string _confidence;
+        public string Confidence
+        {
+            get { return _confidence; }
+            set
+            {
+                _confidence = value;
+                OnPropertyChanged("Confidence");
+            }
+        }
+
+        private string _hypothesizedText;
+        public string HypothesizedText
+        {
+            get { return _hypothesizedText; }
+            set
+            {
+                _hypothesizedText = value;
+                OnPropertyChanged("HypothesizedText");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+        }
+        #endregion
     }
 }
